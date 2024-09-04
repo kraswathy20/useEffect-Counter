940a2ccfebec7653d08ecc42bbd56c34c603b163
Aswathy k.r <aswathy19kr@gmail.com>
2024-09-04 22:14:34 +0530

    added useEffect dependencies on useEffect Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,17 +2,23 @@ import { useState ,useEffect} from "react"
 
 export default function Counter(){
     const [count,setCount] = useState(0)
+    const [name,setName] = useState("")
     
     const handleCounter = ()=>{
         setCount(c => c + 1)
     }
+    const handleName = (evt)=>{
+        setName(evt.target.value)
+    }
     useEffect( function myeffect(){
         console.log("Changed the Counter");
-    })
+    },[])
     return(
         <div>
             <h2>{count}</h2>
             <button onClick={handleCounter}>+1</button>
+            <p>{name}</p>
+            <input type="text" name="name" placeholder="Enter Name" value={name} onChange={handleName} />
         </div>
     )
 }
